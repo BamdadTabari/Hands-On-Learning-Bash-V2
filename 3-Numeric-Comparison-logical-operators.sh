4aab6211c5902b0cd79885648eef2e9407ae7936
BamdadTabari <mohammadJavadtabari1024@outlook.com>
2024-09-23 07:13:13 -0400

    3

--- 3-Numeric-Comparison-logical-operators.sh
+++ 3-Numeric-Comparison-logical-operators.sh
@@ -5,4 +5,31 @@
 
 # Operation             ==>    Syntax       ==>    Explanation
 # Equality              ==>  num1 -eq num2  ==> is num1 equal to num2 ?
-# Greater than equal to ==>  num1 -ge num2  ==>  is num1 greater than equal to num2 ?
+# Greater than equal to ==>  num1 -ge num2  ==> is num1 greater than equal to num2 ?
+# Greater than          ==>  num1 -gt num2  ==> is num1 greater than num2
+# Less than equal to    ==>  num1 -le num2  ==> is num1 less than equal to num2
+# Less than             ==>  num1 -lt num2  ==> is num1 less than num2
+# Not Equal to          ==>  num1 -ne num2  ==> is num1 not equal to num2
+
+
+
+# Syntax:
+
+# if [ conditions ]
+#     then
+#          commands
+# fi
+
+read -r -p " give use the X number : " x
+read -r -p " give use the Y number : " y
+
+if [ $x -gt $y ]
+then
+echo X is greater than Y
+elif [ $x -lt $y ]
+then
+echo X is less than Y
+elif [ $x -eq $y ]
+then
+echo X is equal to Y
+fi
