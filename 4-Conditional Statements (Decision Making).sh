17126dfc99a7eaefe284f38c1c6cc9332fe219fd
BamdadTabari <mohammadJavadtabari1024@outlook.com>
2024-09-25 04:09:36 -0400

    0

--- 4-Conditional Statements (Decision Making).sh
+++ 4-Conditional Statements (Decision Making).sh
@@ -3,3 +3,10 @@
 
 # https://www.freecodecamp.org/news/shell-scripting-crash-course-how-to-write-bash-scripts-in-linux/
 # Conditional Statements (Decision Making)
+
+# Conditions are expressions that evaluate to a boolean expression (true or false). 
+# To check conditions, we can use if, if-else, if-elif-else and nested conditionals.
+
+# if...then...fi statements | if...then...else...fi statements 
+# if..elif..else..fi |   if..then..else..if..then..fi..fi.. (Nested Conditionals)
+
